31f8e264908d56d4efd69ca2820f3ecdb41ad54e
codexjoshy <ebhoriajosh@gmail.com>
2022-06-09 15:08:47 +0100

    initiallize project

---
+++ src/App.js
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="App">
+      <h1>Facetally App</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/assets/index.js
@@ -0,0 +1,33 @@
+import homeHero from './home_hero.png';
+import javascript from './javascript.png';
+import react from './react.png';
+import scene from './scene.png';
+import facebook from './facebook.svg';
+import twitter from './twitter.svg';
+import android from './android.png';
+import apple from './apple.png';
+import frame from './frame.png';
+import mock1 from './mock1.png';
+import mock3 from './mock3.png';
+import mockProfile from './mock-profile.png';
+import mockSwipe from './mock-swipe.png';
+import mockUploads from './mock-uploads.png';
+import mockMatch from './mock-match.png';
+
+export {
+  homeHero,
+  javascript,
+  react,
+  scene,
+  facebook,
+  twitter,
+  android,
+  apple,
+  frame,
+  mock1,
+  mock3,
+  mockProfile,
+  mockSwipe,
+  mockUploads,
+  mockMatch,
+};
