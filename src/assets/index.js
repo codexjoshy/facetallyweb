e3522e81cb77de0722d57ed573081657b04e14d9
Adediran Adeola <35856417+adexdsamson@users.noreply.github.com>
2022-06-10 02:57:55 +0100

    updates: download from app store button

--- src/assets/index.js
+++ src/assets/index.js
@@ -4,15 +4,15 @@
 // import scene from "./scene.png";
 // import facebook from "./facebook.svg";
 // import twitter from "./twitter.svg";
-// import android from "./android.png";
-// import apple from "./apple.png";
+import android from "./android.png";
+import apple from "./apple.png";
 // import frame from "./frame.png";
 // import mock1 from "./mock1.png";
 // import mock3 from "./mock3.png";
 // import mockProfile from "./mock-profile.png";
 // import mockSwipe from "./mock-swipe.png";
 // import mockUploads from "./mock-uploads.png";
-// import mockMatch from "./mock-match.png";
+import mockMatch from "./mock-match.png";
 import logo from "./logo.png";
 import logoSvg from "./logo.svg";
 
@@ -25,13 +25,13 @@ export {
   // scene,
   // facebook,
   // twitter,
-  // android,
-  // apple,
+  android,
+  apple,
   // frame,
   // mock1,
   // mock3,
   // mockProfile,
   // mockSwipe,
   // mockUploads,
-  // mockMatch,
+  mockMatch,
 };
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import { logoSvg } from "../assets";
+import { Container } from "./Container";
 
 export const Navbar = () => {
   const navLinks = [
@@ -25,24 +26,29 @@ export const Navbar = () => {
   ];
 
   return (
-    <nav className="w-full flex items-center justify-between py-5 px-20 ">
-      <div>
-        <img src={logoSvg} alt="logo" className="h-10 w-full" />
-      </div>
-      <div className="flex gap-10 items-center">
-        <ul className="flex items-center justify-center gap-4 pl-3">
-          {navLinks.map((item) => (
-            <li key={item.id}>
-              <a href={item.link} className="text-white/70">
-                {item?.name}
-              </a>
-            </li>
-          ))}
-        </ul>
-        <button className="p-3 bg-black/70 rounded-md text-white/80 ring-1 ring-black">
-          Download
-        </button>
-      </div>
+    <nav className="w-full">
+      <Container
+        as="div"
+        className="w-full flex items-center justify-between py-5"
+      >
+        <div>
+          <img src={logoSvg} alt="logo" className="h-10 w-full" />
+        </div>
+        <div className="flex gap-10 items-center">
+          <ul className="flex items-center justify-center gap-4 pl-3">
+            {navLinks.map((item) => (
+              <li key={item.id}>
+                <a href={item.link} className="text-white/70">
+                  {item?.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <button className="p-3 bg-black/70 rounded-md text-white/80 ring-1 ring-black">
+            Download
+          </button>
+        </div>
+      </Container>
     </nav>
   );
 };
